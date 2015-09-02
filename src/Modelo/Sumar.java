b54c4c7096f19ad660e53a7ac82909a8e187b699
LDS <LDS@LDS03>
2015-09-02 18:15:06 -0500

    Cambio realizado por Walter 26/08/2015

---
+++ src/Modelo/Sumar.java
@@ -0,0 +1,9 @@
+package Modelo;
+
+public class Sumar {
+double datoa, datob, resultado;
+    public double sumar( double datoa, double datob){
+        resultado = datoa + datob;
+        return resultado;
+    }
+}
---
+++ src/umlabp1/UMLABP1.java
@@ -0,0 +1,10 @@
+package umlabp1;
+
+import umlabp1.UI.Formulario;
+
+public class UMLABP1 {
+public static void main(String[] args) {
+      Formulario formulario1 = new Formulario();
+      formulario1.setVisible(true);
+  }
+}
